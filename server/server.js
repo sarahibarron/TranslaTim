5be9daebbc2473ceaf4badcf247c7772e3088a32
SARAH BARRON <sarahibarron@hotmail.co.uk>
2023-11-02 10:36:39 +0000

    wooo

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
+const axios = require("axios");
 
-app.get(PORT, (_, response) => response.json("Root route for translatim."));
+app.get("/", (_, response) => response.json("Root route for translatim."));
 
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
